c478b99367e8d42120a8c8be3b74cfb87d0cba78
William Kent <wjk011@gmail.com>
2021-12-30 15:07:55 -0500

    Add System.Xaml project

---
+++ src/System.Xaml/AssemblyAttributes.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows.Markup;
+
+// Do not compile this file until the below member has been implemented.
+// It is here so I do not forget to apply it later.
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml", "System.Windows.Markup")]
